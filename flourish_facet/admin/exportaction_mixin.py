93f80e23317b3522de72ef305af3eb5343a51321
Keba462 <motlhabi462@gmail.com>
2024-10-22 15:09:53 +0200

    Remove visit,appointment and clinician notes models

--- flourish_facet/admin/exportaction_mixin.py
+++ flourish_facet/admin/exportaction_mixin.py
@@ -126,6 +126,16 @@ class ExportActionMixin(AdminExportHelper):
     def is_visit(self, obj):
         visit_cls = django_apps.get_model('flourish_facet.facetvisit')
         return isinstance(obj, visit_cls)
+    
+    def is_appointment(self, obj):
+        appointment_cls = django_apps.get_model('flourish_facet.appointment')
+        return isinstance(obj, appointment_cls)
+    def is_facet_clinician_notes(self, obj):
+        facet_clinician_cls = django_apps.get_model('flourish_facet.facetcliniciannotes')
+        return isinstance(obj, facet_clinician_cls)
+    def is_clinician_notes_image(self, obj):
+        clinician_cls= django_apps.get_model('flourish_facet.cliniciannotesimage')
+        return isinstance(obj, clinician_cls)
 
     @property
     def get_model_fields(self):
@@ -194,6 +204,9 @@ class ExportActionMixin(AdminExportHelper):
         self.model = model
         try:
             for obj in model.objects.all():
+                result_dict = self.check_all_methods(obj)
+                if any(result for result in result_dict.values()):
+                    continue
                 record = self.process_object_fields(obj)
                 prefixed_record = {
                     f"{model_name}_{key}": value for key, value in record.items()}
@@ -203,3 +216,20 @@ class ExportActionMixin(AdminExportHelper):
             self.model = None
 
         return data
+    
+    def check_all_methods(self, obj):
+        # List of method names to check
+        method_names = ['is_visit', 'is_appointment', 'is_facet_clinician_notes', 'is_clinician_notes_image']
+
+        # Dictionary to store results
+        results = {}
+
+        for method_name in method_names:
+            # Use getattr to dynamically call the method
+            method = getattr(self, method_name, None)
+            
+            if method and callable(method):
+                # Call the method with obj and store the result
+                results[method_name] = method(obj)
+        
+        return results
